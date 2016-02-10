14ab18b0046d15828a3aa0b05486eba3ccadbcbd
Jordi Aguilar <jaguilar-nice@github.com>
2016-02-10 11:51:25 +0100

    Example-2

---
+++ javascript/Examples/extractingInfoFromThePlayer/plugin.js
@@ -0,0 +1,48 @@
+    $YB.plugins.Html5 = function(playerId, options) {
+        // Name and version the plugin
+        this.pluginName = 'html5';
+        this.pluginVersion = '6.0.0-html5';
+
+        // This method will start the library logic.
+        this.init(playerId, options);
+
+        // Register the listeners.
+        this.registerListeners();
+    };
+
+    // Inherit from generic plugin
+    $YB.plugins.Html5.prototype = new $YB.plugins.Generic;
+
+    // Register listeners
+    $YB.plugins.Html5.prototype.registerListeners = function() {
+        var context = this;
+        // Remember: 'this.player' is a reference to the player/video tag.
+        this.player.addEventListener("play", function() {
+            // Once the event is fired, we call hist event handler
+            context.playHandler();
+        });
+
+        this.player.addEventListener("playing", function(e) {
+            context.playingHandler();
+        });
+
+        this.player.addEventListener("pause", function(e) {
+            context.pauseHandler();
+        });
+
+        this.player.addEventListener("ended", function(e) {
+            context.endedHandler();
+        });
+    };
+
+    $YB.plugins.Html5.prototype.getMediaDuration = function() {
+        return this.player.duration;
+    };
+
+    $YB.plugins.Html5.prototype.getPlayhead = function() {
+        return this.player.currentTime;
+    };
+
+    $YB.plugins.Html5.prototype.getResource = function() {
+        return this.player.currentSrc;
+    };
