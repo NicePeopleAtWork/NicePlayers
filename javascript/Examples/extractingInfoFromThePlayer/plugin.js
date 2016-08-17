7e87a3693005a3d0b654ae273975dd567914d066
Jordi Aguilar <jaguilar@nicepeopleatwork.com>
2016-08-17 10:26:45 +0200

    JS examples updated with 5.3.02 lib

--- javascript/Examples/extractingInfoFromThePlayer/plugin.js
+++ javascript/Examples/extractingInfoFromThePlayer/plugin.js
@@ -1,48 +1,48 @@
-    $YB.plugins.Html5 = function(playerId, options) {
-        // Name and version the plugin
-        this.pluginName = 'html5';
-        this.pluginVersion = '6.0.0-html5';
+$YB.plugins.Html5 = function(playerId, options) {
+  // Name and version the plugin
+  this.pluginName = 'html5';
+  this.pluginVersion = '5.3.0-1.0-html5';
 
-        // This method will start the library logic.
-        this.init(playerId, options);
+  // This method will start the library logic.
+  this.startMonitoring(playerId, options);
 
-        // Register the listeners.
-        this.registerListeners();
-    };
+  // Register the listeners.
+  this.registerListeners();
+};
 
-    // Inherit from generic plugin
-    $YB.plugins.Html5.prototype = new $YB.plugins.Generic;
+// Inherit from generic plugin
+$YB.plugins.Html5.prototype = new $YB.plugins.Generic;
 
-    // Register listeners
-    $YB.plugins.Html5.prototype.registerListeners = function() {
-        var context = this;
-        // Remember: 'this.player' is a reference to the player/video tag.
-        this.player.addEventListener("play", function() {
-            // Once the event is fired, we call hist event handler
-            context.playHandler();
-        });
+// Register listeners
+$YB.plugins.Html5.prototype.registerListeners = function() {
+  var context = this;
+  // Remember: 'this.player' is a reference to the player/video tag.
+  this.player.addEventListener("play", function() {
+    // Once the event is fired, we call hist event handler
+    context.playHandler();
+  });
 
-        this.player.addEventListener("playing", function(e) {
-            context.playingHandler();
-        });
+  this.player.addEventListener("playing", function(e) {
+    context.playingHandler();
+  });
 
-        this.player.addEventListener("pause", function(e) {
-            context.pauseHandler();
-        });
+  this.player.addEventListener("pause", function(e) {
+    context.pauseHandler();
+  });
 
-        this.player.addEventListener("ended", function(e) {
-            context.endedHandler();
-        });
-    };
+  this.player.addEventListener("ended", function(e) {
+    context.endedHandler();
+  });
+};
 
-    $YB.plugins.Html5.prototype.getMediaDuration = function() {
-        return this.player.duration;
-    };
+$YB.plugins.Html5.prototype.getMediaDuration = function() {
+  return this.player.duration;
+};
 
-    $YB.plugins.Html5.prototype.getPlayhead = function() {
-        return this.player.currentTime;
-    };
+$YB.plugins.Html5.prototype.getPlayhead = function() {
+  return this.player.currentTime;
+};
 
-    $YB.plugins.Html5.prototype.getResource = function() {
-        return this.player.currentSrc;
-    };
+$YB.plugins.Html5.prototype.getResource = function() {
+  return this.player.currentSrc;
+};
--- javascript/Examples/joinTimeSeekAndBuffer/plugin.js
+++ javascript/Examples/joinTimeSeekAndBuffer/plugin.js
@@ -1,10 +1,13 @@
 $YB.plugins.Html5 = function(playerId, options) {
-    // Name and version the plugin
-    this.pluginName = 'html5';
-    this.pluginVersion = '6.0.0-html5';
+  // Name and version the plugin
+  this.pluginName = 'html5';
+  this.pluginVersion = '5.3.0-1.0-html5';
 
-    // This method will start the library logic.
-    this.init(playerId, options);
+  // This method will start the library logic.
+  this.startMonitoring(playerId, options);
+
+  // Register the listeners.
+  this.registerListeners();
 };
 
 // Inherit from generic plugin
@@ -12,39 +15,39 @@ $YB.plugins.Html5.prototype = new $YB.plugins.Generic;
 
 // Register listeners
 $YB.plugins.Html5.prototype.registerListeners = function() {
-    var context = this;
+  var context = this;
 
-    this.player.addEventListener("play", function() {
-        context.playHandler();
-    });
+  this.player.addEventListener("play", function() {
+    context.playHandler();
+  });
 
-    this.player.addEventListener("playing", function(e) {
-        context.playingHandler();
-    });
+  this.player.addEventListener("playing", function(e) {
+    context.playingHandler();
+  });
 
-    this.player.addEventListener("pause", function(e) {
-        context.pauseHandler();
-    });
+  this.player.addEventListener("pause", function(e) {
+    context.pauseHandler();
+  });
 
-    this.player.addEventListener("ended", function(e) {
-        context.endedHandler();
-    });
+  this.player.addEventListener("ended", function(e) {
+    context.endedHandler();
+  });
 
-    this.player.addEventListener("seeking", function(e) {
-        context.seekingHandler();
-    });
+  this.player.addEventListener("seeking", function(e) {
+    context.seekingHandler();
+  });
 
-    this.startAutobuffer();
+  this.enableBufferMonitor();
 };
 
 $YB.plugins.Html5.prototype.getMediaDuration = function() {
-    return this.player.duration;
+  return this.player.duration;
 };
 
 $YB.plugins.Html5.prototype.getPlayhead = function() {
-    return this.player.currentTime;
+  return this.player.currentTime;
 };
 
 $YB.plugins.Html5.prototype.getResource = function() {
-    return this.player.currentSrc;
+  return this.player.currentSrc;
 };
